83d2bc4fe057bb43e31db45ea25409698ed96383
Aleksandra Garlikowska <garlikowska@gmail.com>
2019-02-19 16:51:25 +0100

    try to write fs.readdir

--- index.js
+++ index.js
@@ -22,5 +22,10 @@ fs.readdir(testFolder, (err, files) => {
     files.forEach(file => {
         console.log(file);
     });
+    const data = new Uint8Array(Buffer.from('Hello Node.js'));
+    fs.writeFile('message.txt', data, (err) => {
+        if (err) throw err;
+        console.log('The file has been saved!');
+    });
 
 });
